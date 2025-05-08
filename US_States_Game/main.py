6299af393e50365b5984d6ba75b644cca7057d0d
Suhaas <53684099+Suhaas15@users.noreply.github.com>
2025-05-08 12:12:57 -0700

    Update main.py
    used a list comprehension instead of a regular for loop

--- US_States_Game/main.py
+++ US_States_Game/main.py
@@ -16,10 +16,10 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        states_to_learn = []
-        for state in all_states:
-            if state not in guessed_states:
-                states_to_learn.append(state)
+        states_to_learn = [state for state in all_states if state not in guessed_states]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         states_to_learn.append(state)
         new_data = pandas.DataFrame(states_to_learn)
         new_data.to_csv("states_to_learn.csv")
         break
